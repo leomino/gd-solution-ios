52ed0d4cd3e0077f9c9545767b1068b5012c868a
Leonardo Palomino <l.palomino1@icloud.com>
2024-05-29 17:07:14 +0200

    fix: position, in search for user in leaderboard

--- gd-solution-ios/Components/Leaderboards/CommunityView.swift
+++ gd-solution-ios/Components/Leaderboards/CommunityView.swift
@@ -7,117 +7,15 @@
 
 import SwiftUI
 
-class UserSuggestionViewModel: LoadingStateModel<[User]> {
-    let dataService: UsersDataServiceProtocol
-    @Published var usernameFilter = ""
-    
-    init(dataService: UsersDataServiceProtocol) {
-        self.dataService = dataService
-        super.init(state: .idle)
-        
-        $usernameFilter.debounce(for: .milliseconds(200), scheduler: DispatchQueue.main)
-            .receive(on: DispatchQueue.main)
-            .sink { [weak self] usernameFilter in
-                guard !usernameFilter.isEmpty else {
-                    return
-                }
-                self?.requests.send(dataService.fetchBy(usernameFilter: usernameFilter))
-            }
-            .store(in: &cancellables)
-    }
-}
-
-class UsersViewModel: LoadingStateModel<[User]> {
-    let dataService: UsersDataServiceProtocol
-    
-    init(dataService: UsersDataServiceProtocol) {
-        self.dataService = dataService
-        super.init(state: .idle)
-    }
-    
-    func inviteToCommunity(communityId: Community.ID, userIds: [User.ID]) {
-        requests.send(dataService.inviteToCommunity(communityId: communityId, userIds: userIds))
-    }
-}
-
-struct InviteToCommunityView: View {
-    @Environment(\.dismiss) var dismiss
-    @ObservedObject var suggestionViewModel: UserSuggestionViewModel
-    @ObservedObject var userViewModel: UsersViewModel
-    @State private var userSelection = Set<String>()
-    let communityId: Community.ID
-    let onInviteSucces: ([User]) -> Void
-    
-    init(communityId: Community.ID, usersDataService: UsersDataServiceProtocol = UsersDataService(), onInviteSucces: @escaping ([User]) -> Void) {
-        self.communityId = communityId
-        self.onInviteSucces = onInviteSucces
-        _suggestionViewModel = ObservedObject(wrappedValue: .init(dataService: usersDataService))
-        _userViewModel = ObservedObject(wrappedValue: .init(dataService: usersDataService))
-    }
-    
-    var body: some View {
-        VStack {
-            TextField("Username", text: $suggestionViewModel.usernameFilter)
-                .autocorrectionDisabled()
-                .textInputAutocapitalization(.never)
-                .textFieldStyle(.roundedBorder)
-            switch suggestionViewModel.state {
-            case .idle:
-                EmptyView()
-            case .loading:
-                ProgressView()
-            case .success(let suggestions):
-                List(suggestions, selection: $userSelection) { user in
-                    HStack {
-                        Text(user.name)
-                        Spacer()
-                        Text(user.username)
-                    }
-                }
-                .listStyle(.plain)
-                .environment(\.editMode, .constant(.active))
-            case .failure(let error):
-                Label(error.localizedDescription, systemImage: "exclamationmark.triangle")
-            }
-            Spacer()
-            switch suggestionViewModel.state {
-            case .loading:
-                ProgressView()
-            case .failure(let error):
-                Label(error.localizedDescription, systemImage: "exclamationmark.triangle")
-            default:
-                EmptyView()
-            }
-            Button("Einladen") {
-                userViewModel.inviteToCommunity(communityId: communityId, userIds: Array(userSelection))
-            }
-            .disabled(userSelection.isEmpty)
-            .buttonStyle(.borderedProminent)
-        }
-        .padding()
-        .onChange(of: userViewModel.state) {
-            if case .success(let invitedUsers) = userViewModel.state {
-                onInviteSucces(invitedUsers)
-                dismiss.callAsFunction()
-            }
-        }
-    }
-}
-
-#Preview {
-    NavigationStack {
-        InviteToCommunityView(communityId: UUID(), usersDataService: UsersDataServiceMock()) { _ in }
-    }
-}
-
 import Combine
 class UserLocalSuggestionViewModel: ObservableObject {
-    @Published var filtered: [User]
+    @Published var filtered: [(User, Int)]
     @Published var userFilter = ""
     public var cancellables = Set<AnyCancellable>()
     
     init(data: [User]) {
-        self.filtered = data
+        let leaderBoardData = data.sorted(by: { $0.points < $1.points }).enumerated().map { ($1, $0) }
+        self.filtered = leaderBoardData
         
         $userFilter.debounce(for: .milliseconds(200), scheduler: DispatchQueue.main)
             .receive(on: DispatchQueue.main)
@@ -126,11 +24,11 @@ class UserLocalSuggestionViewModel: ObservableObject {
                     return
                 }
                 guard !filter.isEmpty else {
-                    self.filtered = data
+                    self.filtered = leaderBoardData
                     return
                 }
-                self.filtered = data.filter {
-                    $0.username.lowercased().contains(filter.lowercased()) || $0.name.lowercased().contains(filter.lowercased())
+                self.filtered = leaderBoardData.filter { (user, _) in
+                    user.username.lowercased().contains(filter.lowercased()) || user.name.lowercased().contains(filter.lowercased())
                 }
             }
             .store(in: &cancellables)
@@ -155,8 +53,8 @@ struct CommunityView: View {
                 .textFieldStyle(.roundedBorder)
                 .padding(.horizontal)
             Spacer()
-            List(Array(userSuggestionViewModel.filtered.sorted(by: { $0.points < $1.points }).enumerated()), id: \.offset) { index, member in
-                LeaderBoardListEntry(member: member, position: index)
+            List(userSuggestionViewModel.filtered, id: \.0) { (member, position) in
+                LeaderBoardListEntry(member: member, position: position)
             }
             .listStyle(.plain)
         }
